3927d1f7ffc76be38cc52293f97f1e58a5476cb9
nitka975 <nikitalim.kh1@gmail.com>
2025-03-15 18:04:05 +0100

    New project from template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Template</h1>
+      <p>Started template for React-project</p>
+    </div>
+  );
+}
+
+export default App;
